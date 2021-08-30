71c7dc190b0c3dce58f1971bd85a6d9499b34b1b
Trista Runions <t.runions19@gmail.com>
2021-08-29 22:35:13 -0700

    finished project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import Character from './components/Character'
+import axios from 'axios'
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [character, setCharacter] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res => {
+      setCharacter(res.data)
+      
+    })
+    .catch(err => console.log(err))
+  })
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character props={character} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,32 @@
-// Write your Character component here
+import React from "react"
+import styled from "styled-components"
+
+
+
+
+export default function Character(props){
+
+    const characterDetails = props.props
+    
+    const newChar = characterDetails.map(i => {
+        return (
+            <MainDiv>
+                <h3>{i.name}</h3>
+                <p>Gender: {i.gender}</p>
+                <p>Birth Year: {i.birth_year}</p>
+                <p>Hair Color: {i.hair_color}</p>
+                <p>Eye Color:{i.eye_color}</p>
+                <p>Height in cm:{i.height}</p>
+            </MainDiv>
+        )
+    })
+    return newChar
+} 
+
+const MainDiv = styled.div`
+  background-color: #e0dcda;
+  margin: 1% 40%;
+  padding: 10px;
+  border: 1px solid #b0a9a5;
+  border-radius: 16px;
+`;
